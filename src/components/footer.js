551308555546c304ff6f10c43efe80f681f25935
shanjng <shan.jiang03@gmail.com>
2021-04-14 17:16:23 -0400

    camelcase for socialMedia import

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
-import SocialMedia from "../components/SocialMedia"
+import SocialMedia from "../components/socialMedia"
 
 const Footer = ({ siteTitle }) => (
   <footer style={{ background: "#eee" }}>
