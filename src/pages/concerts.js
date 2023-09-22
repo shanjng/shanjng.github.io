f7ddded89df37033677a6295b32453deca0abbef
shanjng <shan.jiang03@gmail.com>
2023-09-21 21:41:24 -0700

    Make Concerts Google Sheet take full width of page

--- src/pages/concerts.js
+++ src/pages/concerts.js
@@ -1,5 +1,4 @@
 import * as React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -8,7 +7,9 @@ const Concerts = () => (
   <Layout>
     <SEO title="Concerts" />
     <h1>Concerts I'm Going To</h1>
-    <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vT19rKae6pUJ0PhnQUEzd7wb277rVTcFKdkK3jfiNrXbMDoD5amX01irjKzvUoAbw27rpFTGQzkAVxf/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
+    <div>
+      <iframe width="100%" height="100%" title="Concert Google Sheet" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vT19rKae6pUJ0PhnQUEzd7wb277rVTcFKdkK3jfiNrXbMDoD5amX01irjKzvUoAbw27rpFTGQzkAVxf/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
+    </div>
   </Layout>
 )
 
